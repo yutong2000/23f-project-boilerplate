8d3807ee96b6301f7062df793fd45e09c7f4fb79
ZalaAthrun <135411075+ZalaAthrun@users.noreply.github.com>
2023-12-05 17:42:02 -0500

    flask

---
+++ flask-app/src/restaurant/Restaurant.py
@@ -0,0 +1,56 @@
+from flask import Blueprint, request, jsonify, make_response, current_app
+import json
+from src import db
+
+
+restaurants = Blueprint('restaurants', __name__)
+
+# Get all the products from the database
+@restaurants.route('/restaurant', methods=['GET'])
+def get_restaurant():
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT Availability, Name, Location, Rating')
+    column_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+@restaurants.route('/restaurants/<promotions>', methods=['GET'])
+def get_restaurants_promotions (RestaurantID):
+
+    query = 'Availability, Name, Location, Rating, RestaurantsID FROM Restaurant WHERE RestaurantID = ' + str(RestaurantID)
+    current_app.logger.info(query)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    column_headers = [x[0] for x in cursor.description]
+    json_data = []
+    the_data = cursor.fetchall()
+    for row in the_data:
+        json_data.append(dict(zip(column_headers, row)))
+    return jsonify(json_data)
+    
+
+# get the top 5 products from the database
+@restaurants.route('/foodname')
+def get_manu():
+    cursor = db.get_db().cursor()
+    query = '''
+        SELECT FoodItem, Price
+        FROM restaurants
+        ORDER BY price
+    '''
+    cursor.execute(query)
+    column_headers = [x[0] for x in cursor.description]
+
+    json_data = []
+
+    theData = cursor.fetchall()
+
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
