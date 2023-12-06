27615edeab08bc121b7e198545959cf0e7dd63e8
ZalaAthrun <135411075+ZalaAthrun@users.noreply.github.com>
2023-12-05 21:12:21 -0500

    __init__.py

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -33,8 +33,10 @@ def create_app():
         return "<h1>Welcome to the 3200 boilerplate app</h1>"
 
     # Import the various Beluprint Objects
+    from src.drivers.drivers import drivers
+    from src.admin.admin import admin
+    from src.restaurant.restaurant import restaurant
     from src.customers.customers import customers
-    from src.products.products  import products
 
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each
