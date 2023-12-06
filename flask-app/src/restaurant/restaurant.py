f100d8aace92db1a6891d982dce24d0f5d8189c0
ZalaAthrun <135411075+ZalaAthrun@users.noreply.github.com>
2023-12-06 05:31:43 -0500

    Update restaurant.py

--- flask-app/src/restaurant/restaurant.py
+++ flask-app/src/restaurant/restaurant.py
@@ -36,22 +36,18 @@ def get_restaurants_promotions():
 
 @restaurant.route('/restaurant/menu/<restaurantId>', methods=['GET'])
 def get_menu(restaurantId):
-    cursor = db.get_db().cursor()
-
     query = '''
         SELECT FoodItem_Ava_P.FoodId, FoodItem_Ava_P.FoodName, FoodItem_Ava_P.availability, FoodItem_Ava_P.Price
         FROM FoodItem_Ava_P 
         JOIN Restaurant_foodItem ON FoodItem_Ava_P.FoodId = Restaurant_foodItem.FoodItem
-        WHERE Restaurant_foodItem.RestaurantId = %s
-        ORDER BY FoodItem_Ava_P.Price
-    '''
-    cursor.execute(query, (restaurantId,))
+        WHERE Restaurant_foodItem.RestaurantId = ''' + int(restaurantId)
+    current_app.logger.info(query)
 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
     column_headers = [x[0] for x in cursor.description]
     json_data = []
-    theData = cursor.fetchall()
-
-    for row in theData:
+    the_data = cursor.fetchall()
+    for row in the_data:
         json_data.append(dict(zip(column_headers, row)))
-
     return jsonify(json_data)
