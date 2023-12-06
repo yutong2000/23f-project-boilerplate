81cde6a19c7541673c8865046e3ad118944714dd
ZalaAthrun <135411075+ZalaAthrun@users.noreply.github.com>
2023-12-06 02:25:56 -0500

    Update admin.py

--- flask-app/src/admin/admin.py
+++ flask-app/src/admin/admin.py
@@ -7,7 +7,7 @@ admin = Blueprint('admin', __name__)
 @admin.route('/admin', methods=['GET'])
 def get_admin():
     cursor = db.get_db().cursor()
-    cursor.execute('SELECT Admin.AdminId, Performance, Rating, PhoneNumber, TransactionFee, SupportPolicy, LegalStatus, FeedBackId, FeedBack'
+    cursor.execute('SELECT Admin.AdminId, Performance, Rating, Admin.PhoneNumber, TransactionFee, SupportPolicy, LegalStatus, Admin_FeedBack.FeedBackId, Admin_FeedBack.FeedBack '
                    'join Admin_FeedBack on Admin.AdminId = Admin_FeedBack.Admin.AdminId ')
     column_headers = [x[0] for x in cursor.description]
     json_data = []
