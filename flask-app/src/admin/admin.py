b6b9a22b4ce314e16875ca1ef9ce63747d584d27
ZalaAthrun <135411075+ZalaAthrun@users.noreply.github.com>
2023-12-05 21:08:19 -0500

    admin.py

--- flask-app/src/admin/admin.py
+++ flask-app/src/admin/admin.py
@@ -2,22 +2,24 @@ from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from src import db
 
-
 admin = Blueprint('admin', __name__)
 
 @admin.route('/addrestaurant', methods=['POST'])
 def add_restaurant():
     cursor = db.get_db().cursor()
-    Availability = request.form('Availability')
-    Name = request.form('Name')
-    Rating = request.form('Rating')
-    RestaurantID = request.form('RestaurantID')
-    PhoneNumber = request.form('PhoneNumber')
-    Location = request.form('Location')
-    FoodItem = request.form('FoodItem')
-    Price = request.form('Price')
-    query = 'INSERT INTO Restaurant(Availability, Name, Rating, RestaurantID, PhoneNumber, Location, FoodItem, Price) values(%s, %s, %s, %s, %s, %s, %s, %s)'
-    cursor.execute(query)
+    Availability = request.form.get('Availability')
+    Name = request.form.get('Name')
+    Rating = request.form.get('Rating')
+    RestaurantID = request.form.get('RestaurantID')
+    PhoneNumber = request.form.get('PhoneNumber')
+    Location = request.form.get('Location')
+    FoodItem = request.form.get('FoodItem')
+    Price = request.form.get('Price')
+    query = '''
+        INSERT INTO Restaurant(Availability, Name, Rating, RestaurantID, PhoneNumber, Location, FoodItem, Price) 
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    '''
+    cursor.execute(query, (Availability, Name, Rating, RestaurantID, PhoneNumber, Location, FoodItem, Price))
     db.get_db().commit()
     return 'The restaurant has been added'
 
@@ -36,4 +38,17 @@ def add_driver():
      db.get_db().commit()
      return 'The driver has been added'
 
+@admin.route('/addcustomer', methods=['Post'])
+def add_customer():
+     cursor = db.get_db().cursor()
+     Info = request.form('Info')
+     Address = request.form('Address')
+     PaymentMethod = request.form('PaymentMethod')
+     PhoneNumber = request.form('PhoneNumber')
+     CustomerID = request.form('CustomerID')
+     DeliveryPerference = request.form('DeliveryPerference')
+     query = 'INSERT INTO Customer(Info, CustomerID, Address, PhoneNumber, DeliveryPerference) values(%s, %s, %s, %s, %s)'
+     cursor.execute(query)
+     db.get_db().commit()
+     return 'The costomer has been added'
      
