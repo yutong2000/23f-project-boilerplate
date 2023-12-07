5efcee38c7282d7e534a14a5588f3fb988aae649
ZalaAthrun <135411075+ZalaAthrun@users.noreply.github.com>
2023-12-07 02:33:30 -0500

    Update drivers.py

--- flask-app/src/drivers/drivers.py
+++ flask-app/src/drivers/drivers.py
@@ -95,3 +95,20 @@ def update_driver_availability(driver_id):
     cursor.execute('UPDATE Driver SET Availability = %s WHERE DriverID = %s', (availability, driver_id))
     db.get_db().commit()
     return jsonify({'message': 'Driver availability updated successfully'})
+
+@drivers.route('/driver/order_review/<DriverId>', methods=['GET'])
+def review_order(DriverId):
+    query = '''
+        SELECT Driver_Cus.DriverEarning, Driver_Cus.DriverId, Driver_Cus, DeliveryTime
+        FROM Driver_Cus join Driver on Driver_Cus.DriverId = Driver.DriverId
+        WHERE Driver = 
+    ''' + str(DriverId)
+    current_app.logger.info(query)
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    column_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+    return jsonify(json_data)
